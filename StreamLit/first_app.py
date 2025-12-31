f395f5cfad3c5c2a5ef5ae2df3681b6ba4f51fb6
Subhra-commits <subhradeepmaji13@gmail.com>
2025-12-31 16:53:22 +0530

    Committing streamlit basic concepts - 31 Dec 2025

--- StreamLit/first_app.py
+++ StreamLit/first_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import time
 
 st.write("# My First Streamlit App")
 st.write("Here's our first attempt at using data to create a table:")
@@ -7,4 +9,59 @@ data = pd.DataFrame ({
     'Column 1': [1, 2, 3, 4],
     'Column 2': ['A', 'B', 'C', 'D']
 })
-st.write(data)
+st.table(data)
+st.dataframe(data.style.highlight_max(axis=0))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
+
+option = st.selectbox(
+    'Which number do you like best?',
+     data['Column 1'])
+
+'You selected: ', option
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
