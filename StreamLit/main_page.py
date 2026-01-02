a1b50030d0b1fac9a89d978bd0691524507ee5a8
Subhra-commits <subhradeepmaji13@gmail.com>
2026-01-02 17:04:34 +0530

    Multipagging test in streamlit - 02 Jan 2026

---
+++ StreamLit/main_page.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+# Main page content
+st.markdown("# Main page 🎈")
+st.write("#### Welcome to the main page of the multipage Streamlit app!")
+st.sidebar.markdown("# Main page 🎈")
